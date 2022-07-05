aebb1d30e182e916cdd71d7d52bc632f4d80e89b
abdeladlshaheen <abdeladlshaheen@gmail.com>
2022-07-05 03:55:34 +0200

    update url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://udagram-api-dev3.eba-2j8m4su2.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
